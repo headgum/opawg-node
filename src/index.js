6c43a58341fef71b7fad8d5bdbde5d7537c51d3e
Andrew Pile <bostonia@gmail.com>
2023-05-15 16:07:33 -0400

    Add basic sanity check

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const HOST_DATA = require('../data/hosts.js');
 const PREFIX_DATA = require('../data/prefixes.js');
 
 exports.getHostInfo = (url) => {
+    if (!url) {
+        return null
+    }
+
     const _url = url.toLowerCase().trim();
 
     for (const hostInfo of HOST_DATA) {
@@ -20,6 +24,10 @@ exports.getHostInfo = (url) => {
 }
 
 exports.getPrefixInfo = (url) => {
+    if (!url) {
+        return null
+    }
+
     const _url = url.toLowerCase().trim();
 
     return PREFIX_DATA
