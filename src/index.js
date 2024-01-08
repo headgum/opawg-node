0e17c2c58638293afe7ea33a2b439b207ea45fe7
Peter McArthur <ptrmcrthr@gmail.com>
2024-01-08 17:57:46 +0100

    prefer pattern to rss-pattern

--- src/index.js
+++ src/index.js
@@ -11,12 +11,22 @@ exports.getHostInfo = (url) => {
     const _url = url.toLowerCase().trim();
 
     for (const hostInfo of HOST_DATA) {
-        if (hostInfo['rss-pattern'] && _url.includes(hostInfo['rss-pattern'])) {
-            return hostInfo;
-        }
+        // First, match the host pattern
+        if (url.includes(hostInfo['pattern'])) {
 
-        if (_url.includes(hostInfo['pattern'])) {
-            return hostInfo;
+            // If there is a rss-pattern, match that too. rss-pattern is an optional field that can be used to narrow down the host pattern
+            // https://github.com/opawg/podcast-hosts/blob/55200255683318921d3a368921ee953cb9c865d3/README.md?plain=1#L38
+            if (hostInfo['rss-pattern']) {
+                if (url.includes(hostInfo['rss-pattern'])) {
+                    return hostInfo
+                }
+                
+                // An rss-pattern exists but does not match
+                return null
+            }
+
+            // No rss-pattern, but the host pattern matched
+            return hostInfo
         }
     }
 
